bfc8bcde2636bdb31d4d40db1d10909fc37b4fc5
zlatanoski <zlatanoskidavid92@gmail.com>
2026-05-17 18:11:01 +0200

    added login route in auth and tested it

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -36,3 +36,46 @@ catch (error) {
 }
 
 });
+
+
+router.post('/login',async(req,res)=>{
+    const {email,password} = req.body;
+
+    const cleanEmail = email?.trim().toLowerCase();
+    const cleanPassword = password?.trim();
+
+    if(!cleanEmail || !cleanPassword){
+        return res.status(400).json({error:'All fields are required'})
+    }
+
+    try {
+        const [rows] = await pool.query('SELECT * FROM users WHERE email = ?',[cleanEmail]);
+        if(rows.length === 0){
+            return res.status(401).json({error:'Invalid email or password'})
+        }
+        const validPassword = await bcrypt.compare(cleanPassword,rows[0].password_hash)
+        if(!validPassword){
+            return res.status(401).json({error:'Invalid email or password'})
+        }
+        const token = jwt.sign({userId:rows[0].id},process.env.JWT_SECRET,{expiresIn:'1h'});
+
+        return res.json({token
+        ,user: {  // returning the token and the user details from the user object
+                id: rows[0].id,
+                username: rows[0].username,
+                email: rows[0].email,
+                role: rows[0].role,
+                display_name: rows[0].display_name,
+                avatar_url: rows[0].avatar_url,
+            }
+
+
+
+        })
+    } catch (error) {
+        console.error('Error occurred while checking user:', error);
+        return res.status(500).json({error:'Internal server error'})
+    }
+})
+
+export default router;
