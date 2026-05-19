dfc87cf4f16f6cca4b21ec20d6ee39ab74f7d94a
zlatanoski <zlatanoskidavid92@gmail.com>
2026-05-19 22:07:50 +0200

    feat(frontend) auth api module and bootstrap set up

---
+++ frontend/src/api/auth.js
@@ -0,0 +1,27 @@
+const BASE_URL = "http://localhost:3000/api/auth";
+
+export async function register(username, password) {
+    const response = await fetch(`${BASE_URL}/register`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username, password })
+    });
+    return response.json();
+
+}
+
+
+export async function login(username, password) {
+    const response = await fetch(`${BASE_URL}/login`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username, password })
+    });
+    return response.json();
+}
+
+
