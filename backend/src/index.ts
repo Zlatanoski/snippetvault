2c2deb74c63d9fba4a4e638952c2e41ab08d2732
zlatanoski <zlatanoskidavid92@gmail.com>
2026-09-04 14:35:21 +0200

    feat: add Docker self-hosting support

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -29,7 +29,7 @@ app.use(helmet({
         },
     },
 }));
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 app.use(cors({
     origin: (origin, callback) => {
